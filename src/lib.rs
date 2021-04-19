eb2708a4c442af32460dcc0a1cda2b7d57520db1
Aurimas Blažulionis <0x60@pm.me>
2021-04-19 11:06:44 +0100

    Update to latest memflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,21 @@
 use goblin::Object;
-use memflow::connector::fileio::FileIOMemory;
-use memflow::connector::ConnectorArgs;
+use memflow::connector::fileio::FileIoMemory;
 use memflow::error::*;
 use memflow::mem::MemoryMap;
+use memflow::plugins::Args;
 use memflow::types::{size, Address};
 use std::fs::File;
 use std::io::Read;
 
-#[cfg_attr(feature = "inventory", memflow::derive::connector(name = "kcore"))] 
-pub fn create_connector(args: &ConnectorArgs) -> Result<FileIOMemory<File>> {
+#[cfg_attr(feature = "plugins", memflow::derive::connector(name = "kcore"))]
+pub fn create_connector(args: &Args) -> Result<FileIoMemory<File>> {
     let mut mem = File::open(
         args.get("c")
             .or_else(|| args.get("core"))
             .or_else(|| args.get_default())
-            .map(|v| v.as_str())
             .unwrap_or("/proc/kcore"),
     )
-    .map_err(|_| Error::Other("Failed to open memory"))?;
+    .map_err(|_| Error(ErrorOrigin::Connector, ErrorKind::UnableToReadFile))?;
 
     let mut head = vec![0; size::mb(2)];
     mem.read(&mut head).ok();
@@ -40,8 +39,8 @@ pub fn create_connector(args: &ConnectorArgs) -> Result<FileIOMemory<File>> {
             map.push_remap(b, s, r);
         }
 
-        FileIOMemory::try_with_reader(mem, map)
+        FileIoMemory::try_with_reader(mem, map)
     } else {
-        Err(Error::Other("Failed to parse ELF header"))
+        Err(Error(ErrorOrigin::Connector, ErrorKind::InvalidExeFile))
     }
 }
